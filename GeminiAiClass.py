6b1b2149d6b0792cab8cf409a8f25bace7009750
Felipe Sinnemann <felipe.a.malheiros.s@gmail.com>
2024-12-23 23:18:25 -0300

    Feat: Add new class to use Gemini AI

---
+++ GeminiAiClass.py
@@ -0,0 +1,17 @@
+import google.generativeai as genai
+
+class GeminiAiClass():
+    def __init__(self, apiKey) -> None:
+        genai.configure(api_key=apiKey)
+        self.model = genai.GenerativeModel("gemini-1.5-flash-8b")
+        
+        return
+    
+    def correctText(self, text):
+        inputText = f"""I'm sending to you a a text or phrase in quotation marks in brazilian portuguese. This text or phrase may or not have words that have been erroneously separated, such as "uma" transformed into "u ma" and "Alterar-se" as "Alterar -se". I need you to send me back the exact text or phrase without quotation marks or line breakers but corrected if necessary. Do not replace terms, just correct the errors mentioned. The text is: "{text}" """
+        correctedText = self.model.generate_content(inputText).text
+        return correctedText
+    
+    def finish(self) -> None:
+        self.model.generate_content("It's over for now. Thank you very much for your help.")
+        return
