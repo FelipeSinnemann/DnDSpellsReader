e2d64f5846411de6b5edaef98cc9e137801461f5
Felipe Sinnemann <felipe.a.malheiros.s@gmail.com>
2025-01-10 11:03:39 -0300

    Feat: Interpreting more spells informations

--- SpellClass.py
+++ SpellClass.py
@@ -1,14 +1,22 @@
 class SpellClass():
-    def __init__(self, name: str = None, school: str = None, level: int = None, ritual: bool = None, description: str = None, specialDescription: bool = False) -> None:
+    def __init__(self, name: str = None, school: str = None, level: int = None, ritual: bool = None, castingTime: str = None, range: str = None, verbal: bool = False, somatic: bool = False, material: bool = False, materialComponents: str = None, concentration: bool = False, duration: str = None, description: str = None, specialDescription: bool = False) -> None:
         self.name = name
         self.school = school
         self.level = level
         self.ritual = ritual
+        self.castingTime = castingTime
+        self.range = range
+        self.verbal = verbal
+        self.somatic = somatic
+        self.material = material
+        self.materialComponents = materialComponents
+        self.concentration = concentration
+        self.duration = duration
         self.description = description
         self.specialDescription = specialDescription
     
     def __str__(self) -> str:
-        return f"{self.name}\nLevel: {self.level}\nSchool: {self.school}\nRitual: {self.ritual}\nDescription: {self.description}"
+        return f"{self.name}\nLevel: {self.level}\nSchool: {self.school}\nRitual: {self.ritual}\nCasting time: {self.castingTime}\nRange: {self.range}\nDuration: {self.duration}\nDescription: {self.description}"
     
     def setSpecialDescription(self, adictionalDescription: str) -> None:
         self.specialDescription = True
--- SpellsInterpreter.py
+++ SpellsInterpreter.py
@@ -36,31 +36,93 @@ def __getSpellInfo(spellString: str) -> SpellClass:
         infos[0] = infos[0][1:-1]
         headerFinalIndex -= 1
 
-    name = infos[0].lower().title()
-    spellTipeInfos = __getSpellTipeInfo(infos[1]) 
+    name = infos[0].lower().title().rstrip()
+    spellTipeInfos = __getSpellTipeInfo(infos[1])
+    castingTime = __getSpellCastingTime(infos[2])
+    range = __getSpellRange(infos[3])
+
+    componentsIndex = next((index for index, item in enumerate(infos) if "Componentes" in item), -1)
+    durationIndex = next((index for index, item in enumerate(infos) if "Duração" in item), -1)
+
+    components = __getSpellComponents(__getSpellComponentsString(infos[componentsIndex:durationIndex]))
+    duration = __getSpellDuration(infos[durationIndex])
     description = ''
 
     for descriptionPart in infos[headerFinalIndex+1:]:
         description += descriptionPart
 
-    spell = SpellClass(name, spellTipeInfos['school'], spellTipeInfos['level'], spellTipeInfos['ritual'], description)
+    spell = SpellClass(name = name,
+                        school = spellTipeInfos['school'],
+                          level = spellTipeInfos['level'],
+                            ritual = spellTipeInfos['ritual'],
+                              castingTime = castingTime,
+                                range = range,
+                                  verbal = components['verbal'],
+                                    somatic = components['somatic'],
+                                      material = components['material'],
+                                        materialComponents = components['materialString'],
+                                          concentration = duration['concentration'],
+                                            duration = duration['duration'],
+                                              description = description.rstrip())
     return spell
 
 
-def __getSpellTipeInfo(spellTipeString: str) -> dict :
-    if(spellTipeString.find('Truque') != -1):
-        stringParts = spellTipeString.split('de ')
+def __getSpellTipeInfo(string: str) -> dict :
+    if(string.find('Truque') != -1):
+        stringParts = string.split('de ')
         level = 0
     else:
-        stringParts = spellTipeString.split('nível de ')
+        stringParts = string.split('nível de ')
         level = int(stringParts[0][0])
 
     stringParts = stringParts[1].split(' ')
     ritual = False
 
-    if stringParts[1].find('ritual') != -1:
+    if(stringParts[1].find('ritual') != -1):
         ritual = True
         
     school = stringParts[0].capitalize()
     
     return {"level": level, "school": school, "ritual": ritual}
+
+def __getSpellCastingTime(string: str) -> str :
+    return __getRawInfo(string)
+
+def __getSpellRange(string: str) -> str :
+    return __getRawInfo(string)
+
+def __getSpellComponents(string: str) -> dict :
+    componentsString = __getRawInfo(string)
+
+    verbal = componentsString.find('V') != -1
+    somatic = componentsString.find('S') != -1
+    material = componentsString.find('M') != -1
+    materialString = None
+    if(material):
+        parts = componentsString.split("(")
+        materialString = parts[1]
+        materialString = materialString.split(")")[0].rstrip()
+
+    return {"verbal": verbal, "somatic": somatic, "material": material, "materialString": materialString}
+
+def __getSpellComponentsString(infosComponents: list) -> str :
+    componentsInfo = ''
+    for info in infosComponents:
+        componentsInfo += info
+
+    return componentsInfo
+
+def __getSpellDuration(string: str) -> dict :
+    durationString = __getRawInfo(string)
+
+    concentration = durationString.find('Concentração') != -1
+
+    if(concentration):
+        durationString = durationString.split("até ")[1]
+
+    return {"concentration": concentration, "duration": durationString}
+
+def __getRawInfo(string: str) -> str :
+    stringParts = string.split(' : ')
+
+    return stringParts[1].rstrip()
